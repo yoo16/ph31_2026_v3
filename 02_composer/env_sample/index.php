af2a1fff60d029808963c58a5b543ec6f981c65c
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-09-02 16:10:54 +0900

    update

--- 02_composer/env_sample/index.php
+++ 02_composer/env_sample/index.php
@@ -1,13 +1,13 @@
 <?php
 // TODO: 共通の初期化ファイルを読み込む（Composer オートローダーなど）
-// require_once dirname(__DIR__, 2) . '/bootstrap.php';
+require_once dirname(__DIR__, 2) . '/bootstrap.php';
 
 // TODO: 環境変数を読み込むための Dotenv ライブラリを使用
-// use Dotenv\Dotenv;
+use Dotenv\Dotenv;
 
 // TODO: .env ファイルから環境変数を読み込む
-// $dotenv = Dotenv::createImmutable(__DIR__);
-// $dotenv->load();
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // TODO: 環境変数を定数として定義 $_ENV を使用
 define('API_KEY', "");
